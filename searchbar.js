f7066846777747639f62f1656c74f91a94341db2
Jordan Bailey <jbailey.day@gmail.com>
2019-08-21 10:02:32 -0400

    cmd suggest+autofill, cmd execution

--- searchbar.js
+++ searchbar.js
@@ -1,66 +1,70 @@
 var searchbar, suggbar, sugglist, errspan
 
-var symbol = "/"
-var queryurl = "https://duckduckgo.com/?q="
 var line = { prefix: "", data: "" }
 var error = ""
 
-var savedsites = []
-var suggestions = []
-var hist = []
+var data = {
+    placeholder: "",
+    symbol: "",
+    queryurl: "",
+    savedsites: [],
+    suggestions: [],
+    hist: []
+}
+
+var defaults = {
+    placeholder: "hi there...",
+    symbol: "/",
+    queryurl: "https://duckduckgo.com/?q=",
+    savedsites: [],
+    suggestions: [],
+    hist: []
+}
+
 
 var commands = [
     {
-        name: "scheme",
+        aliases: ["scheme"],
         action: string => {
             
-        },
-        help: ""
+        }
     },
     {
-        name: "add",
+        aliases: ["add"],
         action: string => {
 
-        },
-        help: ""
+        }
     },
     {
-        name: "remove",
+        aliases: ["remove", "rm"],
         action: string => {
 
-        },
-        help: "",
-        aliases: ["rm"]
+        }
     },
     {
-        name: "duckduckgo",
+        aliases: ["duckduckgo", "ddg"],
         action: string => {
-
-        },
-        aliases: ["ddg"]
+            window.location.href = "https://duckduckgo.com/?q=" + string
+        }
     },
     {
-        name: "google",
+        aliases: ["google", "ggl"],
         action: string => {
             window.location.href = "https://www.google.nl/search?q=" + string
         },
-        aliases: ["ggl"]
-    },
-    {
-        name: "queryurl",
-        action: string => {
-            queryurl = string
-        }
     }
 ]
 
 window.addEventListener('DOMContentLoaded', init)
 
 function init() {
-    searchbar = document.getElementById("searchbar")
-    suggbar = document.getElementById("suggbar")
-    sugglist = document.getElementById("suggs")
-    errspan = document.getElementById("err")
+    searchbar = document.querySelector("#searchbar")
+    suggbar = document.querySelector("#suggbar")
+    sugglist = document.querySelector("#suggs")
+    errspan = document.querySelector("#err")
+
+    loadDataOrGetDefault()
+    commands.sort(compareCmds)
 
     window.addEventListener('keydown', handleKeyActions)
     window.addEventListener('mousedown', event => { event.preventDefault(); searchbar.focus() })
@@ -71,43 +75,82 @@ function init() {
 function update(event) {
     event = (event || window.event)
     var target = event.target || event.srcElement
-    // echo(event)
+    //echo(event)
     var input = target.value.trim()
     var dsi = 0 // data start index
-    if (input.startsWith(symbol)) {
+    if (input.startsWith(data.symbol)) {
         var wsi = input.indexOf(" ") // whitespace index
         line.prefix = (wsi != -1 ? input.substring(1, wsi) : input.substring(1))
-    }
-    line.data = input.substring(wsi != -1 ? wsi + 1 : 0)
+    } else line.prefix = ""
 
-    // err ("prefix: " + line.prefix + " data: " + line.data)
+    suggestCommand(line.prefix)
+    line.data = wsi != -1 ? input.substring(wsi + 1) : ""
+
+    err ("prefix: " + line.prefix + " data: " + line.data)
 }
 
 function handleKeyActions(event) {
     switch (event.key) {
         case "Tab":
             event.preventDefault()
+            searchbar.focus()
+            if (suggbar.placeholder != "" && line.data == "") {
+                searchbar.value = suggbar.placeholder
+                suggbar.placeholder = ""
+            }
             break;
         case "Enter":
-            if (line.prefix == "") {
-                search(line.data)
-            } 
+            if (line.prefix == "") search(line.data)
+            else {
+                exec()
+            }
             break;
         default:
             break;
     }
 }
 
-function suggestCommand(cmd) {
 
+// possible change to get command suggestion and used returned value in update function rather than modifying here
+function suggestCommand(prefix) {
+    suggbar.placeholder = ""
+    if (prefix == "") return
+
+    commands.forEach(cmd => {
+        cmd.aliases.forEach(alias => {
+            if (alias.startsWith(prefix)) {
+                suggbar.placeholder = data.symbol + alias
+                return
+            }
+        })
+    });
 }
 
-function exec(cmd) {
-
+function exec() {
+    commands.forEach(cmd => { 
+        if (cmd.aliases.includes(line.prefix)) { 
+            echo(cmd)
+            cmd.action(line.data)
+            return
+        }
+    });
 }
 
 function search(query) {
-    window.location.href = queryurl + query
+    window.location.href = data.queryurl + query
+}
+
+// load data from localStorage or fill with default value if it is not stored
+function loadDataOrGetDefault() {
+    for (const key in defaults) data[key] = localStorage.getItem(key) || defaults[key]
+
+    searchbar.placeholder = data.placeholder
+}
+
+function compareCmds(a, b) {
+    if (a.aliases[0] < b.aliases[0]) return -1
+    if (a.aliases[0] > b.aliases[0]) return 1
+    return 0
 }
 
 function err(str) { errspan.innerHTML = str }
